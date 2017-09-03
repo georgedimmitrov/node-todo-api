d4b1dd2362945ffdbd68ad2176f4e68be0d7fe76
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-03 14:32:58 +0300

    add tests for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -301,3 +301,25 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    const email = users[1].email;
+    const password = users[1].password;
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(e => done(e));
+      });
+  });
+});
